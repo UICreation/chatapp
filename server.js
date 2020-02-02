bba0593d48da6bdd9eb3841cb3c7a79db0ab3c76
shivam.kumar <shivam.designweb@gmail.com>
2020-02-03 02:55:01 +0530

    push to now branch

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 var path = require('path');
 const port = 3001;
+require('dotenv').config({
+    path: __dirname + '/.env'
+})
 // app.use(express.static(__dirname))
 // app.listen(3001, () => 
 // {
 //     console.log('server is listening on port')
 // })
 
-const dbUrl = 'mongodb+srv://shivamkumar:wHWdD4P8Jnvz1viH@learningnode-gwzrq.mongodb.net/test?retryWrites=true&w=majority';
 
 var Message = mongoose.model('Message', {
     name: String,
@@ -29,23 +31,23 @@ app.get('/messages', (req, res) => {
 })
 app.get('/messages/:user', (req, res) => {
     var user = req.params.user
-    Message.find({name: user}, (err, message) => {
+    Message.find({ name: user }, (err, message) => {
         res.send(message)
     })
 })
 app.post('/messages', (req, res) => {
     var message = new Message(req.body)
     message.save()
-    .then(response => {
-        if(response.message == 'badword') {
-            Message.deleteOne({_id: response._id}, err => {
-                console.log('item deleted')
-            })
-            // console.log(response)
-        }
-    })
-        io.emit('message', req.body)
-        res.sendStatus(200)
+        .then(response => {
+            if (response.message == 'badword') {
+                Message.deleteOne({ _id: response._id }, err => {
+                    console.log('item deleted')
+                })
+                // console.log(response)
+            }
+        })
+    io.emit('message', req.body)
+    res.sendStatus(200)
 
 
 })
@@ -67,7 +69,7 @@ io.on('connection', socket => {
     // console.log('a user connected')
 })
 
-mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true }, err => {
+mongoose.connect(process.env.dbURL, { useUnifiedTopology: true, useNewUrlParser: true }, err => {
     console.log('mongodb connected!')
 })
 // app.get('/', (req, res) => res.send('hello shivam kumar'))
